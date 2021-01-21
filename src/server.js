7b59b12b2e17d125098486d5248ded3012fbb123
rasulkurbanov <rasuljohn139@gmail.com>
2021-01-21 16:04:53 +0500

    Add new features

--- src/server.js
+++ src/server.js
@@ -3,31 +3,37 @@ const path = require('path')
 const csv = require('fast-csv')
 
 
+let data1 = [];
+
 function readcsv(csvfile) {
 
   fs.createReadStream(path.resolve(__dirname, 'assets', `${csvfile}`))
     .pipe(csv.parse({ headers: true }))
-    .on('error', (error) => console.log(error))
-    .on('data', (row) => {
-      console.log(JSON.stringify(row))
-    })
-    // .on('end', (rowCount) => console.log(`Parsed ${rowCount} rows`))
-    .on('data', (data) => fs.createWriteStream())
 
+    .on('error', (error) => console.log(error))
+
+    .on('data', (row) =>  {
+      data1.push(row)
+    })
+    .on('end', (rowCount) => console.log(`Parsed ${rowCount} rows`))
 }
+
+// setTimeout(() => {
+//   console.log(data1)
+// }, 2000);
 
 
 function comparecsv(download) {
   readcsv('answers.csv')
-
-  // setTimeout(() => {
-  //   console.log(`Student's answer`)
-  //   readcsv(download)
-  // }, 500); 
-}
-
-comparecsv('download.csv')
-
+  
+  setTimeout(() => {
+      console.log(`Student's answer`)
+      readcsv(download)
+    }, 500); 
+  }
+  
+  comparecsv('download.csv')
+  
 
 
 
